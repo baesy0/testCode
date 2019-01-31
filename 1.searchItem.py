116f7bb3a6d3dbe3c0c4af5158d7d8874ce4d6da
bae <bsy2658@gmail.com>
2019-01-31 16:39:43 +0900

    파일 구조 수정

--- 1.searchItem.py
+++ 1.searchItem.py
@@ -3,13 +3,14 @@
 import os
 
 def searchItem(root):
-	results = []
+	seqs = []
 	for subdir in os.listdir(root):
+		shot = []
 		for f in os.listdir(root+"/"+subdir):
-			results.append("/".join([root,subdir,f]))
-			
-	results.sort()
-	return results
+			shot.append("/".join([root,subdir,f]))
+		shot.sort()
+		seqs.append(shot)
+	return results seqs
 
 if __name__ == "__main__":
 	root = "/project/circle/in/aces_exr"
