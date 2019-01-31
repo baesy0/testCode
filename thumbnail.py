4aa7b539c80caaada58b00cddd9670ae3ab46f6e
bae <bsy2658@gmail.com>
2019-01-31 10:18:18 +0900

    make thumbnail using ImageMagick

---
+++ thumbnail.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#coding:utf8
+import os
+import subprocess
+import sys
+
+def genThumb(src):
+	"""
+	경로를 입력받으면 썸네일을 만든다.
+	"""
+	if not os.path.exists(src):
+		return "", "파일이 존재하지 않습니다."
+	if not os.path.isfile(src):
+		return "", "파일 형태가 아닙니다."
+	if not os.path.exists("/usr/bin/convert"):
+		return None, "ImageMagick이 설치되지 않았습니다."
+
+	d, f = os.path.split(src)
+	notuse, ext = os.path.splitext(f)
+	dst = d+"/"+f.replace(ext, ".jpg")
+	size = "360x240"
+	cmds = ["convert", src, "-thumbnail", size,
+			"-background", "black", "-gravity", "center",
+			"-extent", size, dst]
+
+	
+	p = subprocess.Popen(cmds,
+						stdout = subprocess.PIPE,
+						stderr = subprocess.PIPE)
+	return p.communicate()
+	os.system(cmd)
+	
+if __name__ == "__main__":
+	path = "/project/circle/in/aces_exr/A003C025_150830_R0D0/A003C025_150830_R0D0.078950.exr"
+	stdOut, stdErr = genThumb(path)
+	if stdErr:
+		sys.stderr.write(stdErr)
+	sys.stdout.write(stdOut)
+	
+	
+
+
